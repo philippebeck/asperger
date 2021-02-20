aca7b3fdc520936c65c70f0144b11e7a302c2fa7
Philippe Beck <philippe@philippebeck.net>
2021-02-20 19:11:20 +0100

    Remove useless tables

--- config/db.sql
+++ config/db.sql
@@ -13,26 +13,6 @@ CREATE TABLE `Users`
 )
     ENGINE=INNODB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `Articles`
-(
-  `id`              TINYINT         UNSIGNED    PRIMARY KEY AUTO_INCREMENT,
-  `name`            VARCHAR(30)     NOT NULL    UNIQUE,
-  `created_date`    DATETIME        NOT NULL,
-  `updated_date`    DATETIME        NOT NULL,
-  `content`         TEXT
-)
-ENGINE=INNODB DEFAULT CHARSET=utf8;
-
-CREATE TABLE `Resources`
-(
-  `id`          TINYINT         UNSIGNED    PRIMARY KEY AUTO_INCREMENT,
-  `name`        VARCHAR(30)     NOT NULL    UNIQUE,
-  `link`        VARCHAR(60)     NOT NULL    UNIQUE,
-  `category`    VARCHAR(30)     NOT NULL,
-  `description` TEXT
-)
-ENGINE=INNODB DEFAULT CHARSET=utf8;
-
 CREATE TABLE `Tests`
 (
     `id`          TINYINT       UNSIGNED    PRIMARY KEY AUTO_INCREMENT,
