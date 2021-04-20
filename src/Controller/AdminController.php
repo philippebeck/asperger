4a0b225f948830c87c9c886bd9b45ae3d6bf2c94
Philippe Beck <philippe@philippebeck.net>
2021-04-20 22:12:05 +0200

    Create AdminController

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Controller;
+
+use Pam\Controller\MainController;
+use Pam\Model\Factory\ModelFactory;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+
+/**
+ * Class AdminController
+ * @package App\Controller
+ */
+class AdminController extends MainController
+{
+    /**
+     * @return string
+     * @throws LoaderError
+     * @throws RuntimeError
+     * @throws SyntaxError
+     */
+    public function defaultMethod()
+    {
+        if ($this->getSecurity()->checkIsAdmin() !== true) {
+            $this->redirect("home");
+        }
+
+        $tests      = ModelFactory::getModel("Test")->listData();
+        $aQuestions = ModelFactory::getModel("AQ")->listData();
+        $eQuestions = ModelFactory::getModel("EQ")->listData();
+        $fQuestions = ModelFactory::getModel("FQ")->listData();
+        $sQuestions = ModelFactory::getModel("SQ")->listData();
+        $visitors   = ModelFactory::getModel("Visitor")->listData();
+        $users      = ModelFactory::getModel("User")->listData();
+
+        return $this->render("back/admin/admin.twig", [
+            "tests"         => $tests,
+            "aQuestions"    => $aQuestions,
+            "eQuestions"    => $eQuestions,
+            "fQuestions"    => $fQuestions,
+            "sQuestions"    => $sQuestions,
+            "visitors"      => $visitors,
+            "users"         => $users
+        ]);
+    }
+}
